f4152d7dbb0fd2ff4633aa45874fb698ea5e52d9
KasumiHachiman <vthuongle99@gmail.com>
2017-02-14 23:25:16 -0800

    the first commit

---
+++ MyShop.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WebApp.Startup))]
+namespace MyShop.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        { 
+            ConfigureAuth(app);
+        }
+    }
+}
